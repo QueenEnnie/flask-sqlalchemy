4d3e2ad4f999f586063a131c00059a1ea8a86b36
Ksenia <kcy.2006@mail.ru>
2023-03-26 23:07:23 +0300

    login

---
+++ main.py
@@ -0,0 +1,90 @@
+import datetime
+from data import db_session
+from flask import Flask
+from flask import render_template, redirect
+
+from data.users import User
+from data.jobs import Jobs
+from data.departments import Department
+
+from data.login_form import LoginForm
+from data.users_form import RegisterForm
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    all_jobs = session.query(Jobs).all()
+    users_dict = {}
+    for elem in session.query(User).all():
+        users_dict[elem.id] = elem.surname + " " + elem.name
+    return render_template("index.html", jobs=all_jobs, users=users_dict)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(surname=form.surname.data, name=form.name.data, email=form.email.data,
+                    age=form.age.data, position=form.position.data, speciality=form.speciality.data,
+                    address=form.address.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
