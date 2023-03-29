a0261cccf51d282bdba57afeea35e4870bc25843
Ksenia <kcy.2006@mail.ru>
2023-03-29 19:53:52 +0300

    test.py

--- data/test.py
+++ data/test.py
@@ -1,3 +1,12 @@
 from requests import get
 
 print(get('http://localhost:5000/api/jobs').json())
+print()
+
+print(get('http://localhost:5000/api/jobs/1').json())
+print()
+
+print(get('http://localhost:5000/api/jobs/34').json())
+print()
+
+print(get('http://localhost:5000/api/jobs/h').json())
