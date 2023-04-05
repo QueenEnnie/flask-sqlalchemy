4e526c71380ce85f8e597d85a10e97df72c89463
Ksenia <kcy.2006@mail.ru>
2023-04-05 19:51:18 +0300

    testing

--- data/test.py
+++ data/test.py
@@ -1,20 +1,11 @@
 from requests import get, post
 
-# print(get('http://localhost:5000/api/jobs').json())
-# print()
-#
-# print(get('http://localhost:5000/api/jobs/1').json())
-# print()
-#
-# print(get('http://localhost:5000/api/jobs/34').json())
-# print()
-#
-# print(get('http://localhost:5000/api/jobs/h').json())
+print(post('http://localhost:5000/api/jobs').json())  # ничего не отправлляется
 
-print(post('http://localhost:5000/api/jobs').json())
+print(post('http://localhost:5000/api/jobs', json={}).json())  # отправляется пустой json-файл
 
 print(post('http://localhost:5000/api/jobs',
-           json={'team_leader': 5}).json())
+           json={'team_leader': 5}).json())  # отправляется json-файл с недостаточным количеством данным
 
 print(post('http://localhost:5000/api/jobs',
            json={'team_leader': 2,
@@ -22,4 +13,13 @@ print(post('http://localhost:5000/api/jobs',
                  'collaborators': '3, 4, 5',
                  'is_finished': False,
                  'work_size': 26,
-                 'id': 7}).json())
+                 'id': 7}).json())  # отправляется json-файл с уже существующим id
+
+print(post('http://localhost:5000/api/jobs',
+           json={'team_leader': 1,
+                 'job': 'development of new schedule',
+                 'collaborators': '1, 2',
+                 'is_finished': True,
+                 'work_size': 39}).json())  # корректный запрос
+
+print(get('http://localhost:5000/api/jobs').json())  # получение всех работ
