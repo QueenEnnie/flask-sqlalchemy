84c175cfc4d5544b553d057602dc50e05b0dfb37
Ksenia <kcy.2006@mail.ru>
2023-04-05 19:10:45 +0300

    adding a job (rest-api 4)

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,6 +1,6 @@
 import flask
 
-from flask import jsonify
+from flask import jsonify, request
 
 from data import db_session
 from data.jobs import Jobs
@@ -33,3 +33,25 @@ def get_one_job(job_id):
         {'job': job.to_dict(only=('id', 'team_leader', 'job',
                                      'work_size', 'collaborators',
                                      'start_date', 'end_date', 'is_finished'))})
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_job():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job',
+                                     'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    job = Jobs()
+    job.team_leader = request.json["team_leader"]
+    job.job = request.json["job"]
+    job.work_size = request.json["work_size"]
+    job.collaborators = request.json["collaborators"]
+    job.is_finished = request.json["is_finished"]
+    job.start_date = request.json.get("start_date")
+    job.end_date = request.json.get("end_date")
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
--- data/test.py
+++ data/test.py
@@ -1,12 +1,24 @@
-from requests import get
+from requests import get, post
 
-print(get('http://localhost:5000/api/jobs').json())
-print()
+# print(get('http://localhost:5000/api/jobs').json())
+# print()
+#
+# print(get('http://localhost:5000/api/jobs/1').json())
+# print()
+#
+# print(get('http://localhost:5000/api/jobs/34').json())
+# print()
+#
+# print(get('http://localhost:5000/api/jobs/h').json())
 
-print(get('http://localhost:5000/api/jobs/1').json())
-print()
+print(post('http://localhost:5000/api/jobs').json())
 
-print(get('http://localhost:5000/api/jobs/34').json())
-print()
+print(post('http://localhost:5000/api/jobs',
+           json={'team_leader': 5}).json())
 
-print(get('http://localhost:5000/api/jobs/h').json())
+print(post('http://localhost:5000/api/jobs',
+           json={'team_leader': 2,
+                 'job': 'life search',
+                 'collaborators': '3, 4, 5',
+                 'is_finished': False,
+                 'work_size': 26}).json())
